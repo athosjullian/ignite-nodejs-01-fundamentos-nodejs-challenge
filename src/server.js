18082b422cfc538be9fbcb0aa94ac16cc8356393
Athos Jullian <athosjullian@gmail.com>
2024-03-04 14:35:47 -0300

    Creating routes on server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,30 @@
 import http from 'node:http'
 
-const server = http.createServer((req, res) => {
+const database = []
 
-  res.writeHead(200).end();
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === 'GET' && url == '/tasks') {
+    res
+      .writeHead(200, { 'Content-Type': 'application/json' })
+      .end(JSON.stringify(database))
+  }
+
+  if (method === 'POST' && url == '/tasks') {
+    const task = {
+      "id": 1,
+      "name": "task",
+    }
+
+    database.push(task)
+
+    res.writeHead(201).end()
+  }
+
+  res.end();
 })
 
-server.listen(3334);
+server.listen(3334, () => {
+  console.log('Servidor aberto em http://localhost:3334')
+});
