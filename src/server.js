3531f848e44c18ee2dcf9f49986435ba029d8c17
Athos Jullian <athosjullian@gmail.com>
2024-03-04 23:47:47 -0300

    Create Json buffer middleware

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js';
+
 
 const database = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  await json(req, res)
 
   if (method === 'GET' && url == '/tasks') {
     res
@@ -12,12 +16,15 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === 'POST' && url == '/tasks') {
+    const { name } = req.body
+
     const task = {
       "id": 1,
-      "name": "task",
+      "name": name,
     }
 
     database.push(task)
+    console.log(database)
 
     res.writeHead(201).end()
   }
